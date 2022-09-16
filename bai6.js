ad4f8299b3b776b648da600bf82543674ac6bb3f
sonvp97 <112803870+sonvp97@users.noreply.github.com>
2022-09-16 16:36:32 +0700

    first commit

---
+++ bai6.js
@@ -0,0 +1,22 @@
+for (i=1;i<=6;i++){
+    switch (i){
+        case 1:
+            document.write('&nbsp;&nbsp;&nbsp;&nbsp;**&nbsp;&nbsp;&nbsp;&nbsp;**&nbsp;&nbsp;&nbsp;&nbsp;<br>');
+            break;
+        case 2:
+            document.write('*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>');
+            break;
+        case 3:
+            document.write('*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>');
+            break;
+        case 4:
+            document.write('&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>');
+            break;
+        case 5:
+            document.write('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>');
+            break;
+        case 6:
+            document.write('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>');
+            break;
+    }
+}
